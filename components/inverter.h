5ade4f23a58294f642b94959f007dbadb668fedc
Sergey Repenko <srepenko@gmail.com>
2024-08-09 13:26:51 +0300

    modified:   components/inverter.h 	modified:   esp32s3-example.yaml

---
+++ components/inverter.h
@@ -0,0 +1,15 @@
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/switch/switch.h"
+#include "esphome/components/select/select.h"
+#include "esphome/components/pipsolar/select/pipsolar_select.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace inverter {
+
+}
+}
