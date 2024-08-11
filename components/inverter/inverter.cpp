c75a75f5f97728b2c334d0ac4ba4b6aeabf1ff02
Sergey Repenko <srepenko@gmail.com>
2024-08-11 21:59:51 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -57,6 +57,8 @@ void Inverter::update() {
      if (this->device_protocol_id_) {
           this->device_protocol_id_->publish_state(30);
      }
+     this->write_str("QPIGS\r")
+     QPIGS\r
 }
 
 void Inverter::dump_config() {
