91cce34a8046df34914b45f2eb5409acef96cd8e
Sergey Repenko <srepenko@gmail.com>
2024-08-12 13:25:40 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -10,7 +10,7 @@ namespace inverter {
 
 void Inverter::setup() {
      this->state_ = STATE_IDLE;
-     PollingCommand commands_[2] = {
+     struct PollingCommand commands_[] = {
           {"QPIRI", 0, 0, 4, 0, POLLING_QPIRI},
           {"QPIGS", 1000, 0, 5, 0, POLLING_QPIGS},
      };
