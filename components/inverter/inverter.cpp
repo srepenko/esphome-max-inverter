658e3dbadd271706f15124675b3fa69822ad765a
Sergey Repenko <srepenko@gmail.com>
2024-08-12 20:26:42 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -107,11 +107,11 @@ void Inverter::loop() {
           std::string fc;
           char tmp[READ_BUFFER_LENGTH];
           sprintf(tmp, "%s", this->read_buffer_);
-          const char *cmd = this->used_polling_commands_[this->last_polling_command_].command;
+          const char *cmd = (const uint8_t *)this->used_polling_commands_[this->last_polling_command_].command;
           if (cmd == "QPIRI") {
                ESP_LOGD(TAG, "Decode QPIRI");
                this->state_ = STATE_POLL_DECODED;
-          } else if (*cmd == (uint8_t)"QPI") {
+          } else if (*cmd == "QPI") {
                ESP_LOGD(TAG, "Decode QPI");
                this->state_ = STATE_POLL_DECODED;
           } else {
