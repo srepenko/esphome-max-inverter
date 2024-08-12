e8e36884c11282bff22c2b82360012c99cd65fb3
Sergey Repenko <srepenko@gmail.com>
2024-08-12 10:15:47 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -72,7 +72,7 @@ void Inverter::update() {
      if (this->device_protocol_id_) {
           this->device_protocol_id_->publish_state(30);
      }
-     ESP_LOGI("STATE: %d", this->state_);
+     ESP_LOGI("STATE: %c", this->state_);
      this->write_str("QP"); //IGS\r
      this->state_ == STATE_COMMAND;
 }
