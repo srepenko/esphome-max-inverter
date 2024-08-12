59f8269a5a0a90e9f8ccc6c2590fdea033a7f83c
Sergey Repenko <srepenko@gmail.com>
2024-08-12 13:10:42 +0300

    1

--- components/inverter/inverter.h
+++ components/inverter/inverter.h
@@ -85,12 +85,10 @@ class Inverter : public uart::UARTDevice, public PollingComponent {
         };
 
         PollingCommand used_polling_commands_[15];
-        PollingCommand commands_[] = {
+        PollingCommand commands_[2] = {
             {"QPIRI", 1000, 0, 4, 0, POLLING_QPIRI},
             {"QPIGS", 1000, 0, 5, 0, POLLING_QPIGS}
-        }
-
-        ;
+        };
 };
 }
 }
