ddf9d22c34ec3746fcd9064c6ff268fc77b8da81
Sergey Repenko <srepenko@gmail.com>
2024-08-12 15:06:39 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -93,11 +93,12 @@ void Inverter::loop() {
 }
 
 void Inverter::update() {
-     //for (auto &used_polling_command : this->used_polling_commands_) { 
+     for (auto &used_polling_command : this->used_polling_commands_) { 
           
        //   ESP_LOGD(TAG, "Commands: %s", used_polling_command.command);
+       ESP_LOGD(TAG, "Commands: ");
           
-//     } 
+     } 
 }
 
 void Inverter::dump_config() {
