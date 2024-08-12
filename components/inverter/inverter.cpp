2148d87dc089df14c5f643a1751e8eb7857c51cd
Sergey Repenko <srepenko@gmail.com>
2024-08-12 08:07:50 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -10,29 +10,27 @@ namespace inverter {
 
 void Inverter::setup() {}
 
-int Inverter::readline(int readch, char *buffer, int len)
-  {
-    static int pos = 0;
-    int rpos;
-
-    if (readch > 0) {
-      switch (readch) {
-        case '\n': // Ignore new-lines
-          break;
-        case '\r': // Return on CR
-          rpos = pos;
-          pos = 0;  // Reset position index ready for next time
-          return rpos;
-        default:
-          if (pos < len-1) {
-            buffer[pos++] = readch;
-            buffer[pos] = 0;
+int Inverter::readline(int readch, char *buffer, int len) {
+     static int pos = 0;
+     int rpos;
+     if (readch > 0) {
+          switch (readch) {
+               case '\n': // Ignore new-lines
+                    break;
+               case '\r': // Return on CR
+                    rpos = pos;
+                    pos = 0;  // Reset position index ready for next time
+                    return rpos;
+               default:
+                    if (pos < len-1) {
+                         buffer[pos++] = readch;
+                         buffer[pos] = 0;
+                    }
           }
-      }
-    }
-    // No end of line has been found, so return -1.
-    return -1;
-  }
+     }
+     // No end of line has been found, so return -1.
+     return -1;
+}
 
 void Inverter::loop() {
      age = datetime.datetime.now()
