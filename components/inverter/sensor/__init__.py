e765eb9144f100d4d8b49e5516a158ddd167241a
RSV <srepenko@gmail.com>
2024-08-13 11:04:56 +0300

    1

--- components/inverter/sensor/__init__.py
+++ components/inverter/sensor/__init__.py
@@ -1,15 +1,37 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
+from esphome.const import (
+    CONF_BATTERY_VOLTAGE,
+    CONF_BUS_VOLTAGE,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_VOLTAGE,
+    ICON_CURRENT_AC,
+    UNIT_AMPERE,
+    UNIT_CELSIUS,
+    UNIT_HERTZ,
+    UNIT_PERCENT,
+    UNIT_VOLT,
+    UNIT_VOLT_AMPS,
+    UNIT_WATT,
+)
 
 from .. import CONF_INVERTER_ID, INVERTER_COMPONENT_SCHEMA
 
 DEPENDENCIES = ["uart"]
 
 # QPIRI sensors
+CONF_GRID_RATING_VOLTAGE = "grid_rating_voltage"
+CONF_GRID_RATING_CURRENT = "grid_rating_current"
+
 # QPIGS sensors
+CONF_GRID_VOLTAGE = "grid_voltage"
+CONF_GRID_FREQUENCY = "grid_frequency"
 
 
+# QPI
 CONF_DEVICE_PROTOCOL_ID = "device_protocol_id"
 # QET<CRC><cr>: Query total PV generated energy 
 CONF_TOTAL_PV_GENERATED_ENERGY = "total_pv_generated_energy"
@@ -31,6 +53,31 @@ CONF_DAY_PV_GENERATED_ENERGY = "day_output_load_energy"
 # PBMS<CRC><cr>: BMS message 
 
 TYPES = {
+    CONF_GRID_RATING_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_GRID_RATING_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
+
+
+    CONF_GRID_VOLTAGE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    ),
+    CONF_GRID_FREQUENCY: sensor.sensor_schema(
+        unit_of_measurement=UNIT_HERTZ,
+        icon=ICON_CURRENT_AC,
+        accuracy_decimals=1,
+    ),
+
+
+
     CONF_DEVICE_PROTOCOL_ID: sensor.sensor_schema(
         accuracy_decimals=1,
     ),
