0bd2627864bb9ecb7f7d3e1c23517356c378ce72
RSV <srepenko@gmail.com>
2024-08-13 15:56:48 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -52,7 +52,7 @@ void Inverter::loop() {
           this->state_ = STATE_IDLE;
      }
      if (this->state_ == STATE_POLL_COMPLETE) {
-          ESP_LOGI(TAG, "Recive %d byte: %s", this->read_pos_, this->read_buffer_);
+          //ESP_LOGI(TAG, "Recive %d byte: %s", this->read_pos_, this->read_buffer_);
           if (this->check_incoming_crc_()) {
                if (this->read_buffer_[0] == '(' && this->read_buffer_[1] == 'N' && this->read_buffer_[2] == 'A' &&
                this->read_buffer_[3] == 'K') {
