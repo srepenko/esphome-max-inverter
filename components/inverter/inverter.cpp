c20d78ca570ad0f62692ef89b699632ac92838eb
RSV <srepenko@gmail.com>
2024-08-14 14:42:46 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -187,10 +187,10 @@ uint8_t Inverter::send_next_command_() {
 void Inverter::send_next_poll_() {
   uint16_t crc16;
   if (this->last_polling_command_ == 0) {
-     ESP_LOGD(TAG, "Pool time %d", millis() - this->last_poll_);
      if (millis() - this->last_poll_ < this->update_interval_) { 
           return;
      }
+     ESP_LOGD(TAG, "Pool time %d", millis() - this->last_poll_);
      this->last_poll_ = millis();
   }
   this->last_polling_command_ = (this->last_polling_command_ + 1) % 15;
