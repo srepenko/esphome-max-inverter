bbef340ed8835d08d9149a3636a11c6ab5f2fb95
RSV <srepenko@gmail.com>
2024-08-14 16:49:22 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -111,8 +111,7 @@ void Inverter::loop() {
           char tmp[READ_BUFFER_LENGTH];
           sprintf(tmp, "%s", this->read_buffer_);
           //const char *cmd = (const char *)this->used_polling_commands_[this->last_polling_command_].command;
-          const char *cmd;// = (const char *)this->MAX_commands[this->last_polling_command_].command;
-          memcpy(cmd, this->MAX_commands[this->last_polling_command_].command, this->MAX_commands[this->last_polling_command_].length);
+          const char *cmd = std::string((const char *)this->MAX_commands[this->last_polling_command_].command, 4);
           ESP_LOGD(TAG, "Decode %s - millis: %d", cmd, millis()-this->command_start_millis_);
           if (cmd == "QPIRI") {
                ESP_LOGD(TAG, "Decode QPIRI");
