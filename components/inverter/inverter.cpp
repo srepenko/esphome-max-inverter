7a7dc0c68c3d6952757e1a9115de4093442bb203
RSV <srepenko@gmail.com>
2024-08-14 15:40:29 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -154,7 +154,7 @@ void Inverter::update() {
           }
      } 
 */     
-     ESP_LOGD(TAG, "Size: %d", sizeof(MAX_commands));
+     ESP_LOGD(TAG, "Size: %d", std::size(MAX_commands));
 }
 
 void Inverter::dump_config() {
