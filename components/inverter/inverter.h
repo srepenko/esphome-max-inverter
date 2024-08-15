116bcca0c64721b93b615b6cbdc844cc38726e97
RSV <srepenko@gmail.com>
2024-08-15 14:47:53 +0300

    1

--- components/inverter/inverter.h
+++ components/inverter/inverter.h
@@ -204,10 +204,11 @@ class Inverter : public uart::UARTDevice, public PollingComponent {
 /*
     INVERTER_SWITCH(pv_ok_condition_for_parallel_switch, QPIRI)
     INVERTER_SWITCH(pv_power_balance_switch, QPIRI)
-
-    INVERTER_SELECT(output_source_priority_select, QPIRI)
-    INVERTER_VALUED_SELECT(charging_discharging_control_select, QBATCD, std::string)
 */
+    INVERTER_SELECT(output_source_priority_select, QPIRI)
+
+//    INVERTER_VALUED_SELECT(charging_discharging_control_select, QBATCD, std::string)
+
     void switch_command(const std::string &command);
     void setup() override;
     void loop() override;
