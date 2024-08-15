e6399ed21b9631721414c6bbdf5c87fd69990226
RSV <srepenko@gmail.com>
2024-08-15 10:16:19 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -208,7 +208,7 @@ void Inverter::send_next_poll_() {
 void Inverter::add_polling_command_(const char *command) {
      std::string cmd(command);
      for (auto &used_polling_command : this->MAX_commands) { 
-          std::string str(used_polling_command.command);
+          std::string str((const char *)used_polling_command.command);
           str = str.substr(0, used_polling_command.length); 
           if (str == cmd) {
                used_polling_command.state = 1;
