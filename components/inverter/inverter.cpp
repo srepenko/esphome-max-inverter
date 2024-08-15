cd214365e8a4e9ff8701aebe4543796bf09ec0f0
RSV <srepenko@gmail.com>
2024-08-15 16:31:08 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -190,10 +190,11 @@ void Inverter::loop() {
                     this->output_source_priority_->publish_state(value_output_source_priority_);
                }
                // special for output source priority select
+               /*
                if (this->output_source_priority_select_) {
                     std::string value = esphome::to_string(value_output_source_priority_);
                     this->output_source_priority_select_->map_and_publish(value);
-               } /*
+               } 
                // special for output source priority switches
                if (this->output_source_priority_utility_switch_) {
                     this->output_source_priority_utility_switch_->publish_state(value_output_source_priority_ == 0);
