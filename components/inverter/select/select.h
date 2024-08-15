4a875aae75765aa60fd51525f69956b70a39685a
RSV <srepenko@gmail.com>
2024-08-15 15:32:13 +0300

    1

--- components/inverter/select/select.h
+++ components/inverter/select/select.h
@@ -1,8 +1,6 @@
 #pragma once
-
 #include <utility>
 #include <map>
-
 #include "esphome/components/inverter/inverter.h"
 #include "esphome/components/select/select.h"
 #include "esphome/core/component.h"
