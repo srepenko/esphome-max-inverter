adacea9c5f0c316096176c882fcf6512e40a6bad
RSV <srepenko@gmail.com>
2024-08-15 15:50:00 +0300

    1

--- components/inverter/inverter.h
+++ components/inverter/inverter.h
@@ -40,6 +40,8 @@ struct PollingCommand {
     INVERTER_VALUED_ENTITY_(sensor::Sensor, name, polling_command, value_type)
 #define INVERTER_SWITCH(name, polling_command) INVERTER_ENTITY_(switch_::Switch, name, polling_command)
 #define INVERTER_SELECT(name, polling_command) INVERTER_ENTITY_(inverter::InverterSelect, name, polling_command)
+#define INVERTER_VALUED_SELECT(name, polling_command, value_type) \
+  INVERTER_VALUED_ENTITY_(pipsolar::InverterSelect, name, polling_command, value_type)
 #define INVERTER_BINARY_SENSOR(name, polling_command, value_type) \
     INVERTER_VALUED_ENTITY_(binary_sensor::BinarySensor, name, polling_command, value_type)
 
