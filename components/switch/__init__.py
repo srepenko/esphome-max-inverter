dd5d37cd1e4d3ae5ee4a400723fb640c469918c7
RSV <srepenko@gmail.com>
2024-08-15 14:10:13 +0300

    1

--- components/switch/__init__.py
+++ components/switch/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
 from esphome.const import ICON_POWER
-from .. import CONF_INVERTER_ID, INVERTER_COMPONENT_SCHEMA, inverter_ns
-
+#from .. import CONF_INVERTER_ID, INVERTER_COMPONENT_SCHEMA, inverter_ns
+from .. import CONF_INVERTER_ID, INVERTER_COMPONENT_SCHEMA
 
 DEPENDENCIES = ["uart"]
 
