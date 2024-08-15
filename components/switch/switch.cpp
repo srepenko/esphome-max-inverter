02c899dc8dc7fe849bae5b72e9cbaf2c22e68107
RSV <srepenko@gmail.com>
2024-08-15 13:54:51 +0300

    1

---
+++ components/switch/switch.cpp
@@ -0,0 +1,24 @@
+#include "switch.h"
+#include "esphome/core/log.h"
+#include "esphome/core/application.h"
+
+namespace esphome {
+namespace inverter {
+
+static const char *const TAG = "inverter.switch";
+
+void InverterSwitch::dump_config() { LOG_SWITCH("", "Inverter Switch", this); }
+void InverterSwitch::write_state(bool state) {
+  if (state) {
+    if (this->on_command_.length() > 0) {
+      this->parent_->switch_command(this->on_command_);
+    }
+  } else {
+    if (this->off_command_.length() > 0) {
+      this->parent_->switch_command(this->off_command_);
+    }
+  }
+}
+
+}  // namespace pipsolar
+}  // namespace esphome
---
+++ components/switch/switch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../inverter.h"
+#include "esphome/components/switch/switch.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace inverter {
+class Inverter;
+class InverterSwitch : public switch_::Switch, public Component {
+ public:
+  void set_parent(Inverter *parent) { this->parent_ = parent; };
+  void set_on_command(const std::string &command) { this->on_command_ = command; };
+  void set_off_command(const std::string &command) { this->off_command_ = command; };
+  void dump_config() override;
+
+ protected:
+  void write_state(bool state) override;
+  std::string on_command_;
+  std::string off_command_;
+  Pipsolar *parent_;
+};
+
+}  // namespace pipsolar
+}  // namespace esphome
