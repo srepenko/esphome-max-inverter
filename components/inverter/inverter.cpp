e63562d92f211a7029c32e2be19d89dd766006a1
RSV <srepenko@gmail.com>
2024-08-16 16:13:39 +0300

    1

--- components/inverter/inverter.cpp
+++ components/inverter/inverter.cpp
@@ -298,7 +298,7 @@ void Inverter::loop() {
 
 void Inverter::update() {
 
-     auto time = this->time_->now().strftime();
+     auto time = strftime("%Y-%m-%d %H:%M", this->time_->now());
      //ESP_LOGI(TAG, "Time now: %02d-%02d-%02d %02d:%02d:%02d", time.year, time.month, time.day_of_month, time.hour, time.minute, time.second);
      ESP_LOGI(TAG, "Time now: %s", time);
 }
